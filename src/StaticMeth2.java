58c6d43952cb4814e58b02d51f2ffd975aa5cc2e
csamiselo <chisambwe.samiselo@gmail.com>
2019-06-27 11:00:29 +0200

    All programs now transferred form old Java Katas

--- src/StaticMeth2.java
+++ src/StaticMeth2.java
@@ -1,20 +1,20 @@
 /*
-
-A simple program to demonstrate how to acess static member variables using their class 
+A simple program to demonstrate how to access static member variables using their class
 */
-class StaticMethod{
-static int j=13;
-static int k=55;
+class StaticMethod {
+    static int j = 13;
+    static int k = 55;
 
-static void printVlaues(){
-System.out.print("The value of j is:" + " " + j);
+    static void printVlaues() {
+        System.out.print("The value of j is:" + " " + j);
+    }
 }
-}
- /*   variable can be accessed in
-the same way—by use of the dot operator on the name of the class */
+
+/*   variable can be accessed in
+the same way by use of the dot operator on the name of the class */
 class StaticMeth2 {
-public static void main(String[] args) {
-    StaticMethod.printVlaues();
-    System.out.println("The value of k is :" + " " +  StaticMethod.k);
-}
+    public static void main(String[] args) {
+        StaticMethod.printVlaues();
+        System.out.println("The value of k is :" + " " + StaticMethod.k);
+    }
 }
--- src/Volume.java
+++ src/Volume.java
@@ -3,24 +3,28 @@
 //Calculates the Volume of a given Box based on predefined parameters
 
 
+/*
+A simple Java program to calculate the volume of a Box, by passing values its instance variables
 
+*/
 
 class Box1 {
     double width;
     double height;
     double depth;
 }
-    // This class declares an object of type Box.
+
+// This class declares an object of type Box.
 class Volume {
     public static void main(String args[]) {
-    Box1 mybox = new Box1();
-    double vol;
-    // assign values to mybox's instance variables
-    mybox.width = 10;
-    mybox.height = 20;
-    mybox.depth = 15;
-    // compute volume of box
-    vol = mybox.width * mybox.height * mybox.depth;
-    System.out.println("Volume is " + vol);
+        Box1 mybox = new Box1();
+        double vol;
+        // assign values to mybox's instance variables
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+        // compute volume of box
+        vol = mybox.width * mybox.height * mybox.depth;
+        System.out.println("Volume is " + vol);
     }
-    }
+}
