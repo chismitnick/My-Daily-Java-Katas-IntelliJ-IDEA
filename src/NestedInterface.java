0e93be6110c3e357ae2f3ab00c7aa46a8908cdb9
csamiselo <chisambwe.samiselo@gmail.com>
2019-07-11 16:25:16 +0200

    Nested Interface Example

---
+++ src/NestedInterface.java
@@ -0,0 +1,37 @@
+
+/* A simple Java program to implement nested interfaces   */
+
+// An interface can be declared a member of a class or another interface
+class Alpha {
+    public interface Enclosed {
+
+        boolean NonNegative(int x);
+    }
+}
+
+
+//When a nested interface is used outside of its enclosing
+//scope, it must be qualified by the name of the class or interface of which it is a
+//member.
+                        //Fully qualified interface name Alpha.Enclosed
+class Beta implements Alpha.Enclosed{
+
+    public boolean NonNegative(int x) {
+        return x < 0 ? false :true;
+    }
+}
+
+
+public class NestedInterface {
+    public static void main(String[] args) {
+
+        Alpha.Enclosed alp = new Beta();
+
+        if(alp.NonNegative(12));
+        System.out.println("The number entered is non-negative ");
+
+        if(alp.NonNegative(-9));
+        System.out.println("The number entered is negative and will no be displayed ");
+
+    }
+}
