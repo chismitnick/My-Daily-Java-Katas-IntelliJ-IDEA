8284649157cf4dedb605ba5457700481df990490
csamiselo <chisambwe.samiselo@gmail.com>
2019-09-27 10:12:33 +0200

    Added modification to use Throwable to display a description of the error

--- src/HandleError.java
+++ src/HandleError.java
@@ -13,7 +13,14 @@ public class HandleError {
                 c = r.nextInt();
                 a = 12345 / (b / c);
             } catch (ArithmeticException e) {
-                System.out.println("Division by zero Error has been generated");
+                /** Throwable overrides the toString( ) method (defined by Object) so that it returns a string
+                containing a description of the exception. You can display this description in a println( )
+                statement by simply passing the exception as an argument **/
+
+               /* Using throwable to display a description of the error in the stack trace**/ System.out.println("Exception:" +e);
+
+
+               // System.out.println("Division by zero Error has been generated");
                 a = 0; // set the value of a to zero if an exception occurs and continue
                 // program execution
             }
