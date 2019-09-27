ec6548bc1c6e7736398f88dc157fb5c750170754
csamiselo <chisambwe.samiselo@gmail.com>
2019-09-27 11:08:42 +0200

    Progress and Modifications

--- src/HandleError.java
+++ src/HandleError.java
@@ -13,18 +13,19 @@ public class HandleError {
                 c = r.nextInt();
                 a = 12345 / (b / c);
             } catch (ArithmeticException e) {
-                /** Throwable overrides the toString( ) method (defined by Object) so that it returns a string
-                containing a description of the exception. You can display this description in a println( )
-                statement by simply passing the exception as an argument **/
+                /* Throwable overrides the toString( ) method (defined by Object) so that it returns a string
+                 containing a description of the exception. You can display this description in a println( )
+                 statement by simply passing the exception as an argument **/
 
-               /* Using throwable to display a description of the error in the stack trace**/ System.out.println("Exception:" +e);
+                /* Using throwable to display a description of the error in the stack trace**/
+                System.out.println("Exception:" + e);
 
 
-               // System.out.println("Division by zero Error has been generated");
+                // System.out.println("Division by zero Error has been generated");
                 a = 0; // set the value of a to zero if an exception occurs and continue
                 // program execution
             }
-            System.out.println(" a:" +a);
+            System.out.println(" a:" + a);
         }
     }
 }
