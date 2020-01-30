89a49dfa2f9a66b5f6ede4a0ac9c2f4abca753bd
csamiselo <csamiselo@github.com>
2020-01-30 12:01:23 +0200

    modifications

--- src/EnumAreClassTypes/Cars.java
+++ src/EnumAreClassTypes/Cars.java
@@ -6,7 +6,7 @@ package EnumAreClassTypes;
 //You can give them constructors, add instance variables and methods, and even implement interfaces
 
 enum Cars {
-    Mercedes(9000),Audi(6500),Toyota(4500),Mitsubishi(3500),Chrysler(6500),Ferrari(15000);
+    Mercedes(9000), Audi(6500), Toyota(4500), Mitsubishi(3500), Chrysler(6500), Ferrari(15000);
 
     //instance variable within enum
     private int price; //the price of each vehicle
@@ -17,12 +17,14 @@ enum Cars {
     }
 
     //Overloaded Constructor called when no argument is passed
-    Cars(){
+    Cars() {
         price = -1;
     }
 
     public int getPrice() {
         return price;
     }
-
+    //two restrictions that apply to enumerations are:
+    // An enum cannot be a superclass meaning that an enum cannot be extended
+    //An enum cannot inherit another class
 }
