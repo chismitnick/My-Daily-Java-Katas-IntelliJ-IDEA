99dce380473caf90d1f3ea51c52155136a18577a
csamiselo <chisambwe.samiselo@gmail.com>
2020-06-02 08:09:39 +0200

    Simple Observer pattern Implementation

--- src/DesignPatterns/ObserverPattern/Observer.java
+++ src/DesignPatterns/ObserverPattern/Observer.java
@@ -1,4 +1,7 @@
 package DesignPatterns.ObserverPattern;
 
 public class Observer {
+    public void update(){
+        System.out.println("The value of the flag has changed");
+    }
 }
---
+++ src/DesignPatterns/ObserverPattern/ObserverRun.java
@@ -0,0 +1,18 @@
+package DesignPatterns.ObserverPattern;
+
+public class ObserverRun {
+    public static void main(String[] args) {
+        System.out.println("This is a demonstration of the Observer pattern");
+        Observer observer1 = new Observer();
+        Subject subject1 = new Subject();
+        subject1.register(observer1);
+
+        System.out.println("Set the flag value = 76");
+        subject1.setFlag(76);
+
+
+        System.out.println("Set the flag value to = 200");
+        subject1.setFlag(200);
+        subject1.unregister(observer1);
+    }
+}
