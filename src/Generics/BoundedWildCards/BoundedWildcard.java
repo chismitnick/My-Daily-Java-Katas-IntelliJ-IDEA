e5f3347ac67d0157817f3f589677a944be72ffbe
csamiselo <chisambwe.samiselo@gmail.com>
2020-06-10 12:59:33 +0200

    Progess on bounded wildcards

---
+++ src/Generics/BoundedWildCards/BoundedWildcard.java
@@ -0,0 +1,10 @@
+package Generics.BoundedWildCards;
+//An implementation of bounded wildcards
+//bounded wildcards are useful when creating a generic type
+//that will operate on a class hierarchy
+
+public class BoundedWildcard {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Generics/BoundedWildCards/TwoDimension.java
@@ -0,0 +1,10 @@
+package Generics.BoundedWildCards;
+//Class representing two dimensional coordinates
+public class TwoDimension {
+    int x,y;
+
+    public TwoDimension(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
