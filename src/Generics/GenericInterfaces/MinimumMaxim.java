ccf70e336f64fd561113f04d56472102355a8c7f
csamiselo <chisambwe.samiselo@gmail.com>
2020-06-13 02:56:10 +0200

    Generic Interfaces

---
+++ src/Generics/GenericInterfaces/MinimumMaxim.java
@@ -0,0 +1,10 @@
+package Generics.GenericInterfaces;
+
+
+//Generic interfaces are specified just the way you would generic classes
+
+interface MinimumMaxim<T extends Comparable> {
+    T minimum();
+
+    T maximum();
+}
