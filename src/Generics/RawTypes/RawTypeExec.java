f8a0858229c22b476969c86a18552efaf27290e8
csamiselo <chisambwe.samiselo@gmail.com>
2020-06-18 11:07:10 +0200

    Generic Super Classes

--- src/Generics/RawTypes/RawTypeExec.java
+++ src/Generics/RawTypes/RawTypeExec.java
@@ -1,4 +1,5 @@
 package Generics.RawTypes;
+
 //Demonstrate raw types
 //csamiselo@github.com
 public class RawTypeExec {
@@ -9,13 +10,17 @@ public class RawTypeExec {
 
         //GenRaw<Double> doubleGenRaw = new GenRaw<Double>(57.8);
 
+        /*Limit the use of raw types to those cases in which you must
+          mix legacy code with newer generic code*/
+
         // create raw-types using the 'raw' key word
         GenRaw raw = new GenRaw(57.8);
 
         double v = (Double) raw.getVal();
         System.out.println("The value of the double raw is:" + v);
 
-        System.out.println("The value of the string raw is:" + integerGenRaw.getVal());
+        System.out.println("The value of the integer raw is:" + integerGenRaw.getVal());
+        System.out.println(stringGenRaw.getVal());
     }
 
 }
