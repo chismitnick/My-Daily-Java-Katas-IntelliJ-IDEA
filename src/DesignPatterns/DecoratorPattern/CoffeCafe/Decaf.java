e00ef0b77237744133a0c786efd3cf65f73b6211
csamiselo <chisambwe.samiselo@gmail.com>
2020-08-10 16:06:46 +0200

    Decorator Pattern

---
+++ src/DesignPatterns/DecoratorPattern/CoffeCafe/Decaf.java
@@ -0,0 +1,12 @@
+package DesignPatterns.DecoratorPattern.CoffeCafe;
+//CLass definiing decafinated coffe
+public class Decaf extends Beverage {
+	public Decaf() {
+		description = "Decaf Coffee";
+	}
+ 
+	public double cost() {
+		return 1.05;
+	}
+}
+
